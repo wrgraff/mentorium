cdbaac5467486bbafbbf460ca14c8c18b7b427ac
wrgraff <wrgraff@gmail.com>
2020-09-11 05:23:39 +0300

    Добавляет оформление кода

--- .eleventy.js
+++ .eleventy.js
@@ -1,18 +1,21 @@
+const syntaxHighlight = require('@11ty/eleventy-plugin-syntaxhighlight');
+
 module.exports = (function(eleventyConfig) {
+    eleventyConfig.addPlugin(syntaxHighlight);
     eleventyConfig.setDataDeepMerge(true);
 
-    eleventyConfig.addCollection("tagsList", function(collection) {
+    eleventyConfig.addCollection('tagsList', function(collection) {
         let tagSet = new Set();
         collection.getAll().forEach(function(item) {
-            if( "tags" in item.data ) {
+            if( 'tags' in item.data ) {
                 let tags = item.data.tags;
       
                 tags = tags.filter(function(item) {
                     switch(item) {
                         // this list should match the `filter` list in tags.njk
-                        case "all":
-                        case "article":
-                        case "articles":
+                        case 'all':
+                        case 'article':
+                        case 'articles':
                         return false;
                     }
             
