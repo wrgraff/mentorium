dc84201ecaca43d99470c742d93cd370745ffa8f
wrgraff <wrgraff@gmail.com>
2020-09-12 03:44:20 +0300

    Добавляет обработку видео в статьях

--- .eleventy.js
+++ .eleventy.js
@@ -31,6 +31,46 @@ module.exports = (function(eleventyConfig) {
         return [...tagSet];
     });
 
+    eleventyConfig.addTransform('lazyYouTube', (content, outputPath) => {
+        const articles = /articles\/([a-zA-Z0-9_-]+)\/index\.html/i;
+        const iframes = /\<iframe src\=\"https\:\/\/www\.youtube\.com\/embed\/([a-zA-Z0-9_-]+)\"(.*?)\>\<\/iframe>/ig;
+
+        if (outputPath && outputPath.match(articles)) {
+            content = content.replace(iframes, (match, p1) => {
+                const label = match.match(/aria-label=".*"/i);
+                if (label) {
+                    const labelText = label[0].slice(12, -1);
+                    return (
+                        `<div class="video video_labeled">
+                            <a class="video__link" href="https://youtu.be/${p1}">
+                                <img src="https://img.youtube.com/vi/${p1}/maxresdefault.jpg" alt="${labelText}">
+                                <p class="video__label">
+                                    <svg class="video__ico" height="24" viewBox="0 0 24 24" width="24"><path d="M0 0h24v24H0z" fill="none"/><path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm-2 14.5v-9l6 4.5-6 4.5z" fill="currentColor"/></svg>    
+                                    ${labelText}
+                                </p>
+                            </a>
+                            <button type="button" class="video__button">
+                                <span class="visually-hidden">Воспроизвести видео "${labelText}"</span>
+                            </button>
+                        </div>`
+                    );
+                } else {
+                    return (
+                        `<div class="video">
+                            <a class="video__link" href="https://youtu.be/${p1}">
+                                <img src="https://img.youtube.com/vi/${p1}/maxresdefault.jpg" alt="">
+                            </a>
+                            <button type="button" class="video__button">
+                                <span class="visually-hidden">Воспроизвести видео</span>
+                            </button>
+                        </div>`
+                    );
+                };
+            });
+        }
+        return content;
+    });
+
     return {
         dir: {
             input: 'src',
--- gulpfile.js
+++ gulpfile.js
@@ -4,6 +4,7 @@ const gulp = require('gulp'),
     csso = require('gulp-csso'),
     postcss = require('gulp-postcss'),
     autoprefixer = require('autoprefixer'),
+    babel = require('gulp-babel'),
     rename = require('gulp-rename'),
     imagemin = require('gulp-imagemin'),
     webp = require('gulp-webp'),
@@ -35,6 +36,17 @@ const scss = () => {
         // .pipe(gulp.dest('dist/css/'))
 };
 exports.scss = scss;
+
+const js = () => {
+    return gulp.src('src/js/*.js')
+        .pipe(babel())
+        .pipe(gulp.dest('dist/js/'))
+        .pipe(browserSync.stream());
+        // .pipe(uglify())
+        // .pipe(rename({ suffix: '.min' }))
+        // .pipe(gulp.dest('dist/static/js'))
+};
+exports.js = js;
 
 const img = () => {
     return gulp.src('src/img/**/*.{jpg,png,svg}')
@@ -82,6 +94,7 @@ const watch = () => {
     });
 
     gulp.watch('src/scss/**/*.scss', gulp.series(scss));
+    gulp.watch('src/js/**/*.js', gulp.series(js));
     gulp.watch('src/img/**/*{jpg,png,svg}', gulp.series(img));
     gulp.watch('src/fonts/**/*{woff,woff2}', gulp.series(fonts));
     gulp.watch('src/**/*{html,njk,md,njk}', gulp.series(render, reload));
@@ -93,6 +106,7 @@ const build = gulp.series(
     gulp.parallel(
         render,
         scss,
+        js,
         fonts,
         img
     ),
